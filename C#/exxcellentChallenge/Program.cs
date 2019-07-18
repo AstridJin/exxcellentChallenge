21224ea234c4fc5d65421bcb05beac9fa9c03083
Yujin Wang <astrid.wangyujin@gmail.com>
2019-07-18 07:27:33 +0200

    initial

---
+++ C#/exxcellentChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace exxcellentChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
